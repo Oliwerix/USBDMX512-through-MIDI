df3008ea541f9dadf8f7b936f2245d0a5cbef588
Oliwerix <60409548+Oliwerix@users.noreply.github.com>
2020-01-28 22:54:49 +0100

    initial upload

---
+++ MIDIUSB_through_DMX.ino
@@ -0,0 +1,39 @@
+#include <DmxSimple.h>
+#include "MIDIUSB.h"
+
+
+
+#define dmxPin 3 //which pin is used to output to driver chip
+#define dmxChannels //number of DMX512 channels are used
+#define midiType 0x0B //which MIDI signal is to be interpretted as DMX value (default control change)
+
+
+
+void setup() {
+  Serial.begin(115200); //Serial return
+  DmxSimple.usePin(dmxPin);
+  DmxSimple.maxChannel(dmxChannels);
+}
+
+
+midiEventPacket_t rx;
+void loop() {
+  
+  do {
+    rx = MidiUSB.read();
+    if (rx.header != 0) {
+      
+      Serial.print(rx.header, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte1, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte2, DEC);
+      Serial.print("-");
+      Serial.println(rx.byte3, DEC);
+      
+      if(rx.header == midiType){
+        DmxSimple.write(rx.byte2,rx.byte3*2);
+      }
+    }
+  } while (rx.header != 0);
+}
